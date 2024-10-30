f36676ee42201d61414a10fa94c53f177190c3c3
KTH <pni2396@gmail.com>
2024-10-30 11:50:32 +0900

    feat: Spring boot Init files
    project init

---
+++ Back/MyVoca/src/main/java/com/example/myvoca/MyVocaApplication.java
@@ -0,0 +1,13 @@
+package com.example.myvoca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVocaApplication.class, args);
+	}
+
+}
