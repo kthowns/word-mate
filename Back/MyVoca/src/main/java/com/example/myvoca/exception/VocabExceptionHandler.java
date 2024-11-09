68777a3aeb2fad0a6113a231ca8cbf92549ddbcf
KTH <pni2396@gmail.com>
2024-11-09 22:25:03 +0900

    fix: api exceptionhandler coverage fixed
    - restcontrolleradvice for restcontroller

--- Back/MyVoca/src/main/java/com/example/myvoca/exception/VocabExceptionHandler.java
+++ Back/MyVoca/src/main/java/com/example/myvoca/exception/VocabExceptionHandler.java
@@ -6,7 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import static com.example.myvoca.exception.VocabErrorCode.INTERNAL_SERVER_ERROR;
 
