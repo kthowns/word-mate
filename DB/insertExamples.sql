427c72ad50c6e593025480ee49007922137a828b
KTH <pni2396@gmail.com>
2024-11-19 11:17:43 +0900

    fix: DB schema updated
    - 상세 내역 Notion 명세 페이지 확인

--- DB/insertExamples.sql
+++ DB/insertExamples.sql
@@ -1,14 +1,12 @@
 show tables;
-describe user;
-describe vocab;
-describe word;
-describe vocab_word;
+describe users;
+describe vocabs;
+describe words;
 describe definition;
-describe word_definition;
-describe word_stastics;
+describe word_stats;
 describe theme;
 
-INSERT INTO user (username, password)
+INSERT INTO users (username, password)
 VALUES ('user1', 'password1'),
        ('user2', 'password2'),
        ('user3', 'password3'),
@@ -20,67 +18,43 @@ VALUES ('user1', 'password1'),
        ('user9', 'password9'),
        ('user10', 'password10');
 
-INSERT INTO vocab (user_id, title, description, word_count)
-VALUES (1, '어휘 세트 1', '첫 번째 어휘 세트', 5),
-       (2, '어휘 세트 2', '두 번째 어휘 세트', 7),
-       (3, '어휘 세트 3', '세 번째 어휘 세트', 10),
-       (4, '어휘 세트 4', '네 번째 어휘 세트', 3),
-       (5, '어휘 세트 5', '다섯 번째 어휘 세트', 8),
-       (6, '어휘 세트 6', '여섯 번째 어휘 세트', 6),
-       (7, '어휘 세트 7', '일곱 번째 어휘 세트', 9),
-       (8, '어휘 세트 8', '여덟 번째 어휘 세트', 4),
-       (9, '어휘 세트 9', '아홉 번째 어휘 세트', 2),
-       (10, '어휘 세트 10', '열 번째 어휘 세트', 11);
+INSERT INTO vocabs (user_id, title, description, word_count)
+VALUES (1, '어휘 세트 1', '첫 번째 어휘 세트', 3),
+       (2, '어휘 세트 2', '두 번째 어휘 세트', 2),
+       (3, '어휘 세트 3', '세 번째 어휘 세트', 2),
+       (4, '어휘 세트 4', '네 번째 어휘 세트', 2),
+       (5, '어휘 세트 5', '다섯 번째 어휘 세트', 1),
+       (6, '어휘 세트 6', '여섯 번째 어휘 세트', 0),
+       (7, '어휘 세트 7', '일곱 번째 어휘 세트', 0),
+       (8, '어휘 세트 8', '여덟 번째 어휘 세트', 0),
+       (9, '어휘 세트 9', '아홉 번째 어휘 세트', 0),
+       (10, '어휘 세트 10', '열 번째 어휘 세트', 0);
 
-INSERT INTO word (expression)
-VALUES ('apple'),
-       ('run'),
-       ('happy'),
-       ('quickly'),
-       ('beautiful'),
-       ('banana'),
-       ('jump'),
-       ('sad'),
-       ('slowly'),
-       ('orange');
+INSERT INTO words (vocab_id, expression)
+VALUES (1, 'apple'),
+       (1, 'run'),
+       (1, 'happy'),
+       (2, 'quickly'),
+       (2, 'beautiful'),
+       (3, 'banana'),
+       (3, 'jump'),
+       (4, 'sad'),
+       (4, 'slowly'),
+       (5, 'orange');
 
-INSERT INTO definition (definition, type)
-VALUES ('사과', '명사'),
-       ('달리다', '동사'),
-       ('행복한', '형용사'),
-       ('빠르게', '부사'),
-       ('아름다운', '형용사'),
-       ('바나나', '명사'),
-       ('점프하다', '동사'),
-       ('슬픈', '형용사'),
-       ('천천히', '부사'),
-       ('오렌지', '명사');
+INSERT INTO definition (word_id, definition, type)
+VALUES (1, '사과', 'NOUN'),
+       (2, '달리다', 'VERB'),
+       (3, '행복한', 'ADJECTIVE'),
+       (4, '빠르게', 'ADVERB'),
+       (5, '아름다운', 'ADJECTIVE'),
+       (6, '바나나', 'NOUN'),
+       (7, '점프하다', 'VERB'),
+       (8, '슬픈', 'ADJECTIVE'),
+       (9, '천천히', 'ADVERB'),
+       (10, '오렌지', 'NOUN');
 
-INSERT INTO vocab_word (vocab_id, word_id)
-VALUES (1, 1),
-       (1, 2),
-       (1, 3),
-       (2, 4),
-       (2, 5),
-       (3, 6),
-       (3, 7),
-       (4, 8),
-       (4, 9),
-       (5, 10);
-
-INSERT INTO word_definition (word_id, definition_id)
-VALUES (1, 1),
-       (2, 2),
-       (3, 3),
-       (4, 4),
-       (5, 5),
-       (6, 6),
-       (7, 7),
-       (8, 8),
-       (9, 9),
-       (10, 10);
-
-INSERT INTO word_stastics (word_id, correct_count, incorrect_count, isLearned)
+INSERT INTO word_stats (word_id, correct_count, incorrect_count, is_learned)
 VALUES (1, 10, 2, 1),
        (2, 5, 3, 1),
        (3, 8, 1, 1),
@@ -104,19 +78,9 @@ VALUES (1, 'Arial', 12, '#000000'),
        (9, 'Impact', 26, '#E74C3C'),
        (10, 'Lucida Console', 28, '#3498DB');
 
-select *
-from user;
-select *
-from vocab;
-select *
-from word;
-select *
-from vocab_word;
-select *
-from definition;
-select *
-from word_definition;
-select *
-from word_stastics;
-select *
-from theme;
+select * from users;
+select * from vocabs;
+select * from words;
+select * from definition;
+select * from word_stats;
+select * from theme;
