bbc7a085d778a19a5255e8a20089f664dd24cf76
KTH <pni2396@gmail.com>
2024-12-02 14:16:17 +0900

    fix: 데이터 로딩 API 기반으로 수정 - 세부 모달 기능 확인 필요 - id 관련 변수 그냥 id로 되어있는거 DB 스키마에 맞게 vocabId 형식으로 수정

--- Front/src/App.js
+++ Front/src/App.js
@@ -9,11 +9,27 @@ import OXQuiz from './components/OXquiz';
 import FillIn from './components/fillin';
 import Vocabulary from './components/vocabulary';
 
-function App() {
-  const [vocabularies, setVocabularies] = useState(() => {
-    const savedVocabularies = localStorage.getItem('vocabularies');
-    return savedVocabularies ? JSON.parse(savedVocabularies) : [];
-  });
+async function fetchJson(url, method = 'GET', body = null) {
+  const headers = { 'Content-Type': 'application/json' };
+  const options = { method, headers };
+
+  if (body) {
+    options.body = JSON.stringify(body);
+  }
+
+  const response = await fetch(url, options);
+  const data = await response.json();
+  if (data.status === 200) {
+    return data;
+  } else {
+    console.error("Failed to load " + url);
+    return [];
+  }
+}
+
+function App(uId) {
+  const [vocabularies, setVocabularies] = useState([]);
+  const [userId, setUserId] = useState(1);
   const [showAddModal, setShowAddModal] = useState(false);
   const [showEditModal, setShowEditModal] = useState(false);
   const [newVocab, setNewVocab] = useState({ title: '', description: '' });
@@ -23,8 +39,43 @@ function App() {
     return savedMode ? JSON.parse(savedMode) : false;
   });
 
+  const fetchVocabData = async () => {
+    try {
+      const vocabData = await fetchJson(`/api/vocabs/all?user_id=${userId}`);
+      if (vocabData.status === 200) {
+        const vocabsWithStats = await Promise.all(
+            vocabData.data.map(async (vocab) => {
+              const statData = await fetch(`/api/stats/all?vocab_id=${vocab.vocabId}`)
+                  .then((res) => res.json())
+                  .then((data) => (data.status === 200 ? data.data : []))
+                  .catch((err) => {
+                    console.error("통계 정보 불러오기 실패", err);
+                    return [];
+                  });
+
+              return { ...vocab, stats: statData };
+            })
+        );
+        setVocabularies(vocabsWithStats);
+      } else {
+        console.error("단어장 정보 불러오기 실패");
+        setVocabularies([]);
+      }
+    } catch (error) {
+      console.error('Error fetching vocab data:', error);
+    }
+  };
+
   useEffect(() => {
-    localStorage.setItem('vocabularies', JSON.stringify(vocabularies));
+    fetchVocabData();
+  }, []);
+
+  useEffect(() => {
+    fetchVocabData();
+  }, [userId]);
+
+  useEffect(() => {
+    console.log(vocabularies);
   }, [vocabularies]);
 
   useEffect(() => {
@@ -49,9 +100,11 @@ function App() {
     setShowEditModal(false);
   };
 
-  const handleSubmitVocabulary = () => {
+  const handleSubmitVocabulary = async () => {
     if (newVocab.title) {
-      setVocabularies([...vocabularies, { ...newVocab, id: Date.now(), wordCount: 0 }]);
+      const vocabToAdd = { title: newVocab.title, description: newVocab.description };
+      await fetchJson(`/api/vocabs/${userId}`, 'POST', vocabToAdd);
+      await fetchVocabData();
       setNewVocab({ title: '', description: '' });
       closeAddModal();
     } else {
@@ -59,178 +112,154 @@ function App() {
     }
   };
 
-  const handleUpdateVocabulary = () => {
+  const handleUpdateVocabulary = async () => {
     if (editingVocab) {
-      setVocabularies(
-        vocabularies.map((vocab) =>
-          vocab.id === editingVocab.id ? { ...vocab, ...newVocab } : vocab
-        )
-      );
-      closeEditModal();
+      const updatedVocab = { title: newVocab.title, description: newVocab.description };
+      await fetchJson(`/api/vocabs/${editingVocab.vocabId}`, 'PATCH', updatedVocab);
+      await fetchVocabData();
     }
+    closeEditModal();
   };
 
-  const deleteVocabulary = (id) => {
-    setVocabularies(vocabularies.filter((vocab) => vocab.id !== id));
-  };
-
-  const updateVocabularyWords = (vocabId, newWords) => {
-    setVocabularies(vocabularies.map(vocab => 
-        vocab.id === vocabId 
-            ? { 
-                ...vocab, 
-                words: newWords,
-                wordCount: newWords.length 
-              }
-            : vocab
-    ));
+  const deleteVocabulary = async (id) => {
+    await fetchJson(`/api/vocabs/${id}`, 'DELETE');
+    await fetchVocabData();
   };
 
   return (
-    <Router>
-      <div className={`container ${isDarkMode ? 'dark-mode' : ''}`}>
-        <header>
-          <div className="header-left">
-            <img src={logo} alt="Logo" className="logo" />
-            <button className="add-button" onClick={openAddModal}>Add</button>
-            <button 
-              className={`mode-toggle-button ${isDarkMode ? 'dark' : 'light'}`} 
-              onClick={toggleDarkMode}
-            >
-              {isDarkMode ? 'NIGHT' : 'DAY'}
-            </button>
-          </div>
-          <div className="learning-rate">
-            <p>학습률 통계 섹션</p>
-          </div>
-        </header>
+      <Router>
+        <div className={`container ${isDarkMode ? 'dark-mode' : ''}`}>
+          <header>
+            <div className="header-left">
+              <img src={logo} alt="Logo" className="logo" />
+              <button className="add-button" onClick={openAddModal}>Add</button>
+              <button
+                  className={`mode-toggle-button ${isDarkMode ? 'dark' : 'light'}`}
+                  onClick={toggleDarkMode}>
+                {isDarkMode ? 'NIGHT' : 'DAY'}
+              </button>
+            </div>
+            <div className="learning-rate">
+              <p>학습률 통계 섹션</p>
+            </div>
+          </header>
 
-        <main>
-          <section className="vocab-list">
-            {vocabularies.map((vocab) => (
-              <div key={vocab.id} className="vocab-item">
-                <Link to={`/vocabulary/${vocab.id}`} className="vocab-title">
-                  <h2>{vocab.title} ({vocab.wordCount} 단어)</h2>
-                  <p className="description">{vocab.description}</p>
-                </Link>
-                <div className="vocab-buttons-container">
-                  <div className="vocab-buttons">
-                    <Link to={`/flashcard/${vocab.id}`} className="vocab-button">
-                      플래시 카드
+          <main>
+            <section className="vocab-list">
+              {vocabularies.map((vocab) => (
+                  <div key={vocab.vocabId} className="vocab-item">
+                    <Link to={`/vocabulary/${vocab.vocabId}`} className="vocab-title">
+                      <h2>{vocab.title} ({vocab.wordCount} 개)</h2>
+                      <p className="description">{vocab.description}</p>
                     </Link>
-                    <Link to={`/oxquiz/${vocab.id}`} className="vocab-button">
-                      O/X
-                    </Link>
-                    <Link to={`/fillin/${vocab.id}`} className="vocab-button">
-                      빈칸 채우기
-                    </Link>
+                    <div className="vocab-buttons-container">
+                      <div className="vocab-buttons">
+                        <Link to={`/flashcard/${vocab.vocabId}`} className="vocab-button">
+                          플래시 카드
+                        </Link>
+                        <Link to={`/oxquiz/${vocab.vocabId}`} className="vocab-button">
+                          O/X
+                        </Link>
+                        <Link to={`/fillin/${vocab.vocabId}`} className="vocab-button">
+                          빈칸 채우기
+                        </Link>
+                      </div>
+                      <div className="action-buttons">
+                        <button
+                            className="vocab-button edit-button"
+                            onClick={() => openEditModal(vocab)}>
+                          수정
+                        </button>
+                        <button
+                            className="vocab-button delete-button"
+                            onClick={() => deleteVocabulary(vocab.vocabId)}>
+                          삭제
+                        </button>
+                      </div>
+                    </div>
                   </div>
-                  <div className="action-buttons">
-                    <button
-                      className="vocab-button edit-button"
-                      onClick={() => openEditModal(vocab)}
-                    >
-                      수정
-                    </button>
-                    <button
-                      className="vocab-button delete-button"
-                      onClick={() => deleteVocabulary(vocab.id)}
-                    >
-                      삭제
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </section>
-        </main>
+              ))}
+            </section>
+          </main>
 
-        {showAddModal && (
-          <Modal title="단어장 추가" onClose={closeAddModal}>
-            <label>제목:</label>
-            <input
-              type="text"
-              value={newVocab.title}
-              onChange={(e) => setNewVocab({ ...newVocab, title: e.target.value })}
-              className="input-field"
-            />
-            <label>설명:</label>
-            <textarea
-              value={newVocab.description}
-              onChange={(e) => setNewVocab({ ...newVocab, description: e.target.value })}
-              className="textarea-field"
-            />
-            <button onClick={handleSubmitVocabulary} className="submit-button">
-              제출
-            </button>
-          </Modal>
-        )}
+          {showAddModal && (
+              <Modal title="단어장 추가" onClose={closeAddModal}>
+                <label>제목:</label>
+                <input
+                    type="text"
+                    value={newVocab.title}
+                    onChange={(e) => setNewVocab({ ...newVocab, title: e.target.value })}
+                    className="input-field"
+                />
+                <label>설명:</label>
+                <textarea
+                    value={newVocab.description}
+                    onChange={(e) => setNewVocab({ ...newVocab, description: e.target.value })}
+                    className="textarea-field"
+                />
+                <button onClick={handleSubmitVocabulary} className="submit-button">
+                  제출
+                </button>
+              </Modal>
+          )}
 
-        {showEditModal && (
-          <Modal title="단어장 수정" onClose={closeEditModal}>
-            <label>제목:</label>
-            <input
-              type="text"
-              value={newVocab.title}
-              onChange={(e) => setNewVocab({ ...newVocab, title: e.target.value })}
-              className="input-field"
-            />
-            <label>설명:</label>
-            <textarea
-              value={newVocab.description}
-              onChange={(e) => setNewVocab({ ...newVocab, description: e.target.value })}
-              className="textarea-field"
-            />
-            <button onClick={handleUpdateVocabulary} className="submit-button">
-              수정
-            </button>
-          </Modal>
-        )}
+          {showEditModal && (
+              <Modal title="단어장 수정" onClose={closeEditModal}>
+                <label>제목:</label>
+                <input
+                    type="text"
+                    value={newVocab.title}
+                    onChange={(e) => setNewVocab({ ...newVocab, title: e.target.value })}
+                    className="input-field"
+                />
+                <label>설명:</label>
+                <textarea
+                    value={newVocab.description}
+                    onChange={(e) => setNewVocab({ ...newVocab, description: e.target.value })}
+                    className="textarea-field"
+                />
+                <button onClick={handleUpdateVocabulary} className="submit-button">
+                  수정
+                </button>
+              </Modal>
+          )}
 
-        <Routes>
-          <Route 
-            path="/vocabulary/:id" 
-            element={
-              <Vocabulary 
-                vocabularies={vocabularies}
-                onUpdateVocabulary={updateVocabularyWords}
-                isDarkMode={isDarkMode}
-              />
-            } 
-          />
-          <Route 
-            path="/flashcard/:id" 
-            element={
-              <Flashcard 
-                vocabularies={vocabularies}
-                onUpdateVocabulary={updateVocabularyWords}
-                isDarkMode={isDarkMode}
-              />
-            } 
-          />
-          <Route 
-            path="/oxquiz/:id" 
-            element={
-              <OXQuiz 
-                vocabularies={vocabularies}
-                onUpdateVocabulary={updateVocabularyWords}
-                isDarkMode={isDarkMode}
-              />
-            } 
-          />
-          <Route 
-            path="/fillin/:id" 
-            element={
-              <FillIn 
-                vocabularies={vocabularies}
-                onUpdateVocabulary={updateVocabularyWords}
-                isDarkMode={isDarkMode}
-              />
-            } 
-          />
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route
+                path="/vocabulary/:vocabId"
+                element={
+                  <Vocabulary
+                      isDarkMode={isDarkMode}
+                  />
+                }
+            />
+            <Route
+                path="/flashcard/:vocabId"
+                element={
+                  <Flashcard
+                      isDarkMode={isDarkMode}
+                  />
+                }
+            />
+            <Route
+                path="/oxquiz/:vocabId"
+                element={
+                  <OXQuiz
+                      isDarkMode={isDarkMode}
+                  />
+                }
+            />
+            <Route
+                path="/fillin/:vocabId"
+                element={
+                  <FillIn
+                      isDarkMode={isDarkMode}
+                  />
+                }
+            />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
