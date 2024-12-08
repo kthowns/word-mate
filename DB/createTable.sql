98b733be04334beaf97c6db03cf49c6edd7d4fb0
김태현 <70064360+kthowns@users.noreply.github.com>
2024-12-08 12:35:18 +0900

    Update createTable.sql

--- DB/createTable.sql
+++ DB/createTable.sql
@@ -3,47 +3,42 @@ CREATE USER 'myvoca'@'%' identified by 'password';
 GRANT ALL PRIVILEGES ON *.* TO 'myvoca'@'%';
 USE myvoca;
 
-CREATE TABLE users
-(
-    user_id  INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(32) UNIQUE NOT NULL,
-    password VARCHAR(32)        NOT NULL
+CREATE TABLE users (
+	user_id INT AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(32) UNIQUE NOT NULL,
+	password VARCHAR(32) NOT NULL
 );
 
-CREATE TABLE vocabs
-(
-    vocab_id    INT AUTO_INCREMENT PRIMARY KEY,
-    user_id     INT         NOT NULL,
-    title       VARCHAR(16) NOT NULL,
-    description VARCHAR(32),
-    word_count  INT,
-    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+CREATE TABLE vocabs (
+	vocab_id INT AUTO_INCREMENT PRIMARY KEY,
+	user_id INT NOT NULL,
+	title VARCHAR(16) NOT NULL,
+	description VARCHAR(32),
+	word_count INT,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
-CREATE TABLE words
-(
-    word_id    INT AUTO_INCREMENT PRIMARY KEY,
-    vocab_id   INT         NOT NULL,
-    expression VARCHAR(32) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (vocab_id) REFERENCES vocabs (vocab_id) ON DELETE CASCADE
+CREATE TABLE words (
+	word_id INT AUTO_INCREMENT PRIMARY KEY,
+	vocab_id INT NOT NULL,
+	expression VARCHAR(32) NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	FOREIGN KEY (vocab_id) REFERENCES vocabs(vocab_id) ON DELETE CASCADE
 );
 
-CREATE TABLE stats
-(
-    word_id         INT PRIMARY KEY,
-    correct_count   INT     DEFAULT 0,
-    incorrect_count INT     DEFAULT 0,
-    is_learned      TINYINT DEFAULT 0,
-    FOREIGN KEY (word_id) REFERENCES words (word_id) ON DELETE CASCADE
+CREATE TABLE stats (
+	word_id INT PRIMARY KEY,
+	correct_count INT DEFAULT 0,
+	incorrect_count INT DEFAULT 0,
+	is_learned TINYINT DEFAULT 0,
+	FOREIGN KEY (word_id) REFERENCES words(word_id) ON DELETE CASCADE
 );
 
-CREATE TABLE defs
-(
-    def_id     INT AUTO_INCREMENT PRIMARY KEY,
-    word_id    INT         NOT NULL,
-    definition VARCHAR(64) NOT NULL,
-    type       VARCHAR(32) NOT NULL,
-    FOREIGN KEY (word_id) REFERENCES words (word_id) ON DELETE CASCADE
+CREATE TABLE defs (
+	def_id INT AUTO_INCREMENT PRIMARY KEY,
+	word_id INT NOT NULL,
+	definition VARCHAR(64) NOT NULL,
+	type VARCHAR(32) NOT NULL,
+	FOREIGN KEY (word_id) REFERENCES words(word_id) ON DELETE CASCADE
 );
